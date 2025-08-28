889d174b14fd19a1194b3f3f383456f9ed56153c
root <2470005072@qq.com>
2025-08-29 00:10:47 +0800

    rm install triton deps

--- python/setup.py
+++ python/setup.py
@@ -832,9 +832,6 @@ setup(
         "Programming Language :: Python :: 3.12",
     ],
     test_suite="tests",
-    install_requires=[
-        "triton==3.2.0",
-    ],
     extras_require={
         "build": [
             "cmake>=3.20",
